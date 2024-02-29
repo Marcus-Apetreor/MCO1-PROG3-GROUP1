1bf8bfa91a730993bbcccbc4c2b4de1cb419ae09
Marcus-Apetreor <lee_jacob_apetreor@dlsu.edu.ph>
2024-02-29 16:38:10 +0800

    Update TitleScreen.java

--- TitleScreen.java
+++ TitleScreen.java
@@ -4,7 +4,7 @@ import java.util.*;
  * Class representing the title screen of the game.
  * Allows players to start the game or exit.
  * 
- * Authors: Marcus Apetreor, Vincent Vuelva
+ * @authors: Marcus Apetreor, Vincent Vuelva
  */
 public class TitleScreen {
 
